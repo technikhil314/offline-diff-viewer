873cc8ec8efce1d3092f12937a33e41cf14d3a3f
Nikhil Mehta <nikhil.001mehta@gmail.com>
2022-06-27 08:30:20 +0530

    refactor: moved all buttons to their own components

---
+++ components/buttons/copyLink.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <button
+      type="button"
+      class="inline-flex items-center justify-center gap-1 p-2 text-sm transition-transform transform rounded-md shadow outline-none  justify-self-end focus:ring-4 active:scale-y-75 hover:scale-105 hover:shadow-lg copy-uri-button"
+      aria-label="Copy url to clipboard"
+      :class="{
+        'bg-blue-500 text-white': !copied,
+        'bg-green-500 text-gray-800': copied,
+      }"
+      @click="clickHandler"
+    >
+      <span v-show="copied" class="inline">
+        <svg
+          class="w-4 h-4"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"
+          ></path>
+        </svg>
+      </span>
+      <span v-show="copied" class="hidden md:inline-block">Copied</span>
+      <span v-show="!copied" class="inline">
+        <svg
+          class="w-4 h-4"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
+          ></path>
+        </svg>
+      </span>
+      <span v-show="!copied" class="hidden md:inline-block">Copy link</span>
+    </button>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    clickHandler: {
+      type: Function,
+      required: true,
+    },
+    copied: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
---
+++ components/buttons/nextDiff.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="nextDiffSection" class="inline-flex items-center gap-1">
+    <button
+      id="nextDiff"
+      class="inline-flex items-center justify-center px-1 py-1 text-sm text-gray-600 transition-transform transform bg-gray-300 border border-gray-800 rounded-sm outline-none dark:border-gray-400 dark:text-white dark:bg-gray-800 align-center focus:ring-4 active:scale-y-75 hover:scale-105 hover:shadow-lg"
+      aria-label="Go to next diff"
+      type="button"
+      @click="clickHandler"
+    >
+      Next diff
+      <svg
+        class="w-4 h-4"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M16 17l-4 4m0 0l-4-4m4 4V3"
+        ></path>
+      </svg>
+    </button>
+  </div>
+  </template>
+  <script>
+    export default {
+      props: {
+        clickHandler: {
+          type: Function,
+          required: true,
+        },
+      },
+    }
+  </script>
+</template>
