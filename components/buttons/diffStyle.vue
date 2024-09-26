1fc545066241d367e5126de7a2b3544b4606e622
Nikhil Mehta <6815560+technikhil314@users.noreply.github.com>
2024-09-26 20:47:28 +0530

    fix(a11y): fixed typo in diff style button

--- components/buttons/diffStyle.vue
+++ components/buttons/diffStyle.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="inline-flex items-center justify-center px-1 py-1 text-sm text-gray-600 transition-transform transform bg-gray-300 border border-gray-800 rounded-sm  dark:border-gray-400 dark:text-white dark:bg-gray-800 align-center focus-visible:ring-4 active:scale-y-75"
+    class="inline-flex items-center justify-center px-1 py-1 text-sm text-gray-600 transition-transform transform bg-gray-300 border border-gray-800 rounded-sm dark:border-gray-400 dark:text-white dark:bg-gray-800 align-center focus-visible:ring-4 active:scale-y-75"
     role="button"
     :aria-label="label"
     :title="label"
@@ -20,8 +20,8 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import DiffStyle from '~/components/icons/diffStyle.vue'
+import Vue from "vue";
+import DiffStyle from "~/components/icons/diffStyle.vue";
 export default Vue.extend({
   components: { DiffStyle },
   props: {
@@ -33,21 +33,21 @@ export default Vue.extend({
   data() {
     return {
       sideBySide: true,
-    }
+    };
   },
   computed: {
     label() {
       if (this.sideBySide) {
-        return 'Switch unified diff layout'
+        return "Switch to unified diff layout";
       }
-      return 'Switch to split diff layout'
+      return "Switch to split diff layout";
     },
   },
   methods: {
     handleClick() {
-      this.sideBySide = !this.sideBySide
-      this.clickHandler(this.sideBySide === true)
+      this.sideBySide = !this.sideBySide;
+      this.clickHandler(this.sideBySide === true);
     },
   },
-})
+});
 </script>
